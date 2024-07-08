22317ce6f5af3c81705362bcefee0abadcdd92a6
afbstorm <aurelien.freyling@bstorm.be>
2024-07-08 15:22:46 +0200

    add route pipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BindingsComponent } from './components/Demos/bindings/bindings.component';
+import { PipesComponent } from './components/Demos/pipes/pipes.component';
 
 export const routes: Routes = [
     {path: 'demos', children: [
-        {path: 'bindings', component: BindingsComponent}
+        {path: 'bindings', component: BindingsComponent},
+        {path: 'pipes', component: PipesComponent}
     ]}
 ];
